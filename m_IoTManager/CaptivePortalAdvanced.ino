7275ce4627f5fffc7b106aece24e8334ce885acf
serhii paukov <spspider95@gmail.com>
2024-11-23 16:29:51 +0300

    gzfiles

--- m_IoTManager/CaptivePortalAdvanced.ino
+++ m_IoTManager/CaptivePortalAdvanced.ino
@@ -13,9 +13,10 @@
 #define CONSOLE_PRINTF CONSOLE._PRINTF
 #define DEBUG_PRINTF CONSOLE_PRINTF
 
+#if defined(dnsServer)
 const byte DNS_PORT = 53;
 DNSServer dnsServer;
-
+#endif
 struct ip_addr *IPaddress;
 uint32 uintaddress;
 
@@ -23,7 +24,6 @@ uint32 uintaddress;
 
 IPAddress apIP(192, 168, 4, 1);
 IPAddress netMsk(255, 255, 255, 0);
-
 
 /** Should I connect to WLAN asap? */
 bool connect = false;
@@ -35,7 +35,8 @@ uint8_t status = WL_IDLE_STATUS;
 uint8_t FreeWIFIid[5];
 bool freeWIFIConnected = false;
 
-void connect_as_AccessPoint() {
+void connect_as_AccessPoint()
+{
   delay(1000);
   Serial.print("Configuring access point...");
   /* You can remove the password parameter if you want the AP to be open. */
@@ -44,9 +45,11 @@ void connect_as_AccessPoint() {
   WiFi.mode(WIFI_OFF);    // Prevent use of SDK stored credentials
   WiFi.softAPConfig(apIP, apIP, netMsk);
   WiFi.softAP(softAP_ssid + WiFi.macAddress(), softAP_password);
+#if defined(dnsServer)
   dnsServer.setTTL(0);
   dnsServer.start(IANA_DNS_PORT, "*", apIP);
-  delay( 500 ); // Without delay I've seen the IP address blank
+#endif
+  delay(500); // Without delay I've seen the IP address blank
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
 }
@@ -54,16 +57,21 @@ void captive_setup()
 { // starting void
 
   Captive_server_init();
-  if (load_ssid_pass()) {
+  if (load_ssid_pass())
+  {
     connect = strlen(ssid) > 0; // Request WLAN connect if there is a SSID
-    if (connect) {
+    if (connect)
+    {
       try_MQTT_access = true;
-    } else {
+    }
+    else
+    {
       try_MQTT_access = false;
       connect_as_AccessPoint();
     }
   }
-  else {
+  else
+  {
     try_MQTT_access = false;
     connect_as_AccessPoint();
   }
@@ -72,9 +80,10 @@ void captive_setup()
   server.begin();
 }
 
-void connectWifi(char ssid_that[32], char password_that[32]) {
+void connectWifi(char ssid_that[32], char password_that[32])
+{
   WiFi.disconnect();
-  status = WL_IDLE_STATUS;//обнуляем статус, для подключения
+  status = WL_IDLE_STATUS; // обнуляем статус, для подключения
   WiFi.mode(WIFI_STA);
   delay(1000);
   Serial.println("Connecting as wifi client...");
@@ -85,7 +94,8 @@ void connectWifi(char ssid_that[32], char password_that[32]) {
   delay(100);
   WiFi.begin(ssid_that, strlen(password_that) > 0 ? password_that : nullptr);
   uint8_t attempt = 0;
-  while (WiFi.status() != WL_CONNECTED && attempt < 50) {
+  while (WiFi.status() != WL_CONNECTED && attempt < 50)
+  {
     delay(500);
     Serial.print(".");
     attempt++;
@@ -93,41 +103,45 @@ void connectWifi(char ssid_that[32], char password_that[32]) {
   if (WiFi.waitForConnectResult() != WL_CONNECTED)
     connect_as_AccessPoint();
 }
-//void try_connect_free() {
-//  Serial.println("!!!scan for FREE WIFI!!!");
-//  uint8_t n = WiFi.scanNetworks();//сканируем Wifi сети
-//  if (n > 0) {//если сетей больше одной
-//    uint8_t number = 0;
-//    for (uint8_t i = 0; i < n; i++) {
-//      if (WiFi.encryptionType(i) == ENC_TYPE_NONE)  {//если эта сеть доступна
-//        FreeWIFIid[number] = i;
-//        number++;
-//      }
-//    }
-//    if (number > 0) {
-//      uint8_t ssid_number = random(number - 1);
-//      //Serial.println("random:" + String(ssid_number));
-//      WiFi.SSID(FreeWIFIid[ssid_number]).toCharArray(ssid, sizeof(ssid) - 1);
-//      strncpy( password, "\0", sizeof(password) - 1);
-//      lastConnectTry = onesec;
-//      connect = false;
-//      connectWifi(ssid, password);
-//      freeWIFIConnected = true;
-//    }
-//    else {
-//      Serial.println("no Free Wifi");
-//    }
-//  }
-//}
-void relayRouter() {
-  if (router != 255) {
+// void try_connect_free() {
+//   Serial.println("!!!scan for FREE WIFI!!!");
+//   uint8_t n = WiFi.scanNetworks();//сканируем Wifi сети
+//   if (n > 0) {//если сетей больше одной
+//     uint8_t number = 0;
+//     for (uint8_t i = 0; i < n; i++) {
+//       if (WiFi.encryptionType(i) == ENC_TYPE_NONE)  {//если эта сеть доступна
+//         FreeWIFIid[number] = i;
+//         number++;
+//       }
+//     }
+//     if (number > 0) {
+//       uint8_t ssid_number = random(number - 1);
+//       //Serial.println("random:" + String(ssid_number));
+//       WiFi.SSID(FreeWIFIid[ssid_number]).toCharArray(ssid, sizeof(ssid) - 1);
+//       strncpy( password, "\0", sizeof(password) - 1);
+//       lastConnectTry = onesec;
+//       connect = false;
+//       connectWifi(ssid, password);
+//       freeWIFIConnected = true;
+//     }
+//     else {
+//       Serial.println("no Free Wifi");
+//     }
+//   }
+// }
+void relayRouter()
+{
+  if (router != 255)
+  {
     pinMode(router, OUTPUT);
     digitalWrite(router, digitalRead(router) ^ 1);
     Serial.println("Router relay state:" + String(digitalRead(router), DEC));
   }
 }
-void captive_loop() {
-  if (connect) {
+void captive_loop()
+{
+  if (connect)
+  {
     status = WL_IDLE_STATUS;
     connect = false;
     connectWifi(ssid, password);
@@ -142,92 +156,113 @@ void captive_loop() {
     }
 #endif
   }
-  if ((!try_MQTT_access) && (WiFi.status() == WL_CONNECTED) && (WiFi.getMode() == WIFI_STA)) {
-    if (onesec > no_internet_timer + 300 ) {//пробовать подключится каждые30 сек
-      try_MQTT_access = true; //пробуем подключить MQTT
-      Serial.println("Connect again");
-      no_internet_timer = onesec;
-    }
-  }
+  // if ((!try_MQTT_access) && (WiFi.status() == WL_CONNECTED) && (WiFi.getMode() == WIFI_STA)) {
+  //   if (onesec > no_internet_timer + 300 ) {//пробовать подключится каждые30 сек
+  //     try_MQTT_access = true; //пробуем подключить MQTT
+  //     Serial.println("Connect again");
+  //     no_internet_timer = onesec;
+  //   }
+  // }
   {
 
     uint8_t s = WiFi.status();
 
-    if ( onesec > lastConnectTry + 60 ) {
-      if (((s == 0) || (s == 6) )) { //или нет подключения, или подключен как точка доступа
-        if (WiFi.getMode() != WIFI_AP ) {//
-          WiFi.disconnect();
-          Serial.println ( "Wrong connection" );
-          lastConnectTry = onesec;
-          connect_as_AccessPoint();//должен подключаться если нет подключения
-          relayRouter();//если нет вещания, значит нужно переключить роутер
-          return;
-        }
-        else if (((WiFi.getMode() == WIFI_AP) && (wifi_softap_get_station_num() == 0)) && (wifi_scan) && staReady)
-        {
-          Serial.println("Connect Creditnails");
-          if (load_ssid_pass()) {
-            WiFi.disconnect();
-            connect = true;
-          }
-
-          dnsServer.setTTL(600); // 10 minutes
-          dnsServer.enableForwarder(myHostname, WiFi.dnsIP(0));
-          if (dnsServer.isDNSSet())
-          {
-            CONSOLE_PRINTF("  Forward other lookups to DNS: %s\r\n", dnsServer.getDNS().toString().c_str());
-          }
-          return;
-        }
-        else if ((!internet)  && (geo_enable)) {
-          Serial.println("Status WIFI:" + String(WiFi.status()));
-        }
+    if (onesec > lastConnectTry + 60)
+    {
+      if ((WiFi.status() == WL_DISCONNECTED) && ((wifi_softap_get_station_num() == 0)))
+      {
+        WiFi.disconnect();
+        Serial.println("Wrong connection");
+        lastConnectTry = onesec;
+        connect_as_AccessPoint();
+        relayRouter();
+        return;
       }
+      else if ((WiFi.getMode() == WIFI_AP) && (wifi_softap_get_station_num() == 0) && wifi_scan && staReady)
+      {
+        Serial.println("Connect Credentials");
+        if (load_ssid_pass())
+        {
+          WiFi.disconnect();
+          connect = true;
+        }
+#if defined(dnsServer)
+        dnsServer.setTTL(600); // 10 minutes
+        dnsServer.enableForwarder(myHostname, WiFi.dnsIP(0));
+        if (dnsServer.isDNSSet())
+        {
+          CONSOLE_PRINTF("  Forward other lookups to DNS: %s\r\n", dnsServer.getDNS().toString().c_str());
+        }
+#endif
+        return;
+      }
+      else if (!internet && geo_enable)
+      {
+        Serial.println("Status WIFI: " + String(WiFi.status()));
+      }
+      else if ((WiFi.getMode() == WIFI_AP) && (wifi_softap_get_station_num() != 0))
+      {
+        Serial.print("stations connected as AP:");
+        Serial.println(wifi_softap_get_station_num());
+        return;
+      }
+
       lastConnectTry = onesec;
     }
-    if (status != s) { // WLAN status change
-      Serial.print ( "Status: " );
-      Serial.println ( s );
+    if (status != s)
+    { // WLAN status change
+      Serial.print("Status: ");
+      Serial.println(s);
       status = s;
-      if (s == WL_CONNECTED) {
+      if (s == WL_CONNECTED)
+      {
 
-        try_MQTT_access = true;//можно попытаться подключиться к интернету
+        try_MQTT_access = true; // можно попытаться подключиться к интернету
         /* Just connected to WLAN */
-        Serial.println ( "Connected to " );
-        Serial.println ( ssid );
-        Serial.print ( "IP address: " );
-        Serial.println ( WiFi.localIP() );
+        Serial.println("Connected to ");
+        Serial.println(ssid);
+        Serial.print("IP address: ");
+        Serial.println(WiFi.localIP());
         server.send(200, "text/plain", toStringIp(WiFi.localIP()));
         save_wifiList(String(ssid), String(password));
         setup_ota();
         MDNS.addService("http", "tcp", ipport);
-        if (geo_enable) sendLocationData();
+        if (geo_enable)
+          sendLocationData();
 #if defined(timerAlarm)
         setup_alarm();
 #endif
 #ifdef use_telegram
         setup_telegram();
 #endif
-
-      } else if (s == WL_NO_SSID_AVAIL) {
+      }
+      else if (s == WL_NO_SSID_AVAIL)
+      {
         internet = false;
         try_MQTT_access = false;
         WiFi.disconnect();
       }
-      else if (s == WL_CONNECTED) MDNS.update();
+      else if (s == WL_CONNECTED)
+        MDNS.update();
 #if defined(timerAlarm)
       setup_alarm();
 #endif
     }
   }
+#if defined(dnsServer)
   dnsServer.processNextRequest();
+#endif
   server.handleClient();
 }
-void sendLocationData() {
+void sendLocationData()
+{
   String pos = getHttp("api.2ip.ua/geo.json?ip=");
-  if (pos != "fail") {
+  if (pos != "fail")
+  {
     internet = true;
-  } else {
+  }
+  else
+  {
     internet = false;
     Serial.println("Internet connection failed");
   }
---
+++ make_gz.ps1
@@ -0,0 +1,47 @@
+﻿# Определяем исходную и целевую директории
+$sourceDir = "HTML_data/"
+$destinationDir = "m_IoTManager/data/"
+
+# Создаём целевую директорию, если её нет
+if (!(Test-Path -Path $destinationDir)) {
+    New-Item -ItemType Directory -Path $destinationDir | Out-Null
+}
+
+# Расширения файлов для компрессии
+$fileExtensions = @(".htm", ".html", ".js", ".css")
+
+# Компрессия файлов
+Get-ChildItem -Path $sourceDir -Recurse -File | Where-Object {
+    $fileExtensions -contains $_.Extension.ToLower()
+} | ForEach-Object {
+    # Вычисляем относительный путь от исходной директории
+    $relativePath = $_.FullName.Substring((Get-Location).Path.Length + $sourceDir.Length + 1)
+    $gzippedFilePath = Join-Path $destinationDir ($relativePath + ".gz")
+
+
+    Write-Output $gzippedFilePath
+    # Гарантируем создание необходимых директории
+    $directoryPath = [System.IO.Path]::GetDirectoryName($gzippedFilePath)
+    if (-not (Test-Path $directoryPath)) {
+        New-Item -ItemType Directory -Path $directoryPath -Force | Out-Null
+    }
+
+    $originalFileName = $_.FullName
+    $gzippedFileName = $gzippedFilePath
+
+    # Компрессия файла
+    $inputFile = [System.IO.File]::OpenRead($originalFileName)
+    $outputFile = [System.IO.File]::Create($gzippedFileName)
+    $gzipStream = New-Object System.IO.Compression.GzipStream($outputFile, [System.IO.Compression.CompressionMode]::Compress)
+
+    $buffer = New-Object byte[] 4096
+    while (($bytesRead = $inputFile.Read($buffer, 0, $buffer.Length)) -gt 0) {
+        $gzipStream.Write($buffer, 0, $bytesRead)
+    }
+
+    $gzipStream.Close()
+    $inputFile.Close()
+    $outputFile.Close()
+
+    Write-Output "Compressed: $originalFileName to $gzippedFileName"
+}
