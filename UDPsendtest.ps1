efd61016d0b37c4978f9a2ed22bf60de6cc094c8
Serhii_Paukov <spspider95@gmail.com>
2025-04-28 15:23:20 +0300

    Add UDP client script for sending test messages

---
+++ UDPsendtest.ps1
@@ -0,0 +1,6 @@
+﻿$udpClient = New-Object System.Net.Sockets.UdpClient
+$remoteEndPoint = New-Object System.Net.IPEndPoint([System.Net.IPAddress]::Parse("192.168.1.143"), 4210)
+$message = "0:501"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($message)
+$udpClient.Send($bytes, $bytes.Length, $remoteEndPoint)
+$udpClient.Close()
