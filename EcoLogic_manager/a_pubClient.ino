a98fc9ff478dbd8cc11928375ae9b132b05132f6
serhii paukov <spspider95@gmail.com>
2025-06-22 15:42:30 +0300

    Refactor pin definitions and enhance default restoration logic: update RESET_PIN definition, improve checkAndRestoreDefaults function to wait before restoring defaults, and streamline file restoration process.

--- EcoLogic_manager/a_pubClient.ino
+++ EcoLogic_manager/a_pubClient.ino
@@ -144,23 +144,6 @@ void loop_IOTManager() // that void is in loop section in a main ino file
 void pubClientOneSecEvent() { //this event call each second, for reduce load on esp8266
   if (client.connected())
   {
-    // if (onesec > oldtime + subscr_sec && subscribe_loop < nWidgets)// here I subscribe for a one message in a 1 period of subscribtion
-    // {
-    //   char topic_subscr[30];
-    //   if ((pinmode[subscribe_loop] == 2) || (pinmode[subscribe_loop] == 3) || (pinmode[subscribe_loop] == 5)) {
-    //     snprintf(topic_subscr, sizeof(topic_subscr), "%s/%d", deviceID, subscribe_loop);
-    //     if (client.subscribe(topic_subscr))
-    //     {
-    //       Serial.print(F("subscribe:"));
-    //       Serial.println(topic_subscr);
-    //       subscribe_loop++;
-    //     }
-    //     oldtime = onesec;
-    //   } else {
-    //     subscribe_loop++;
-    //   }
-    // }
-
     if (onesec_255 > mqttspacing_oldtime + mqttspacing )
     {
       char sTopic_ch[20];
